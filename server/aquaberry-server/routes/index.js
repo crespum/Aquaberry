1fa5e52474eb1fc7dfb2b1d7a9442451238465cf
Xabier Crespo Álvarez <xabicrespog@gmail.com>
2016-03-10 09:36:55 +0100

    API active

--- server/aquaberry-server/routes/index.js
+++ server/aquaberry-server/routes/index.js
@@ -1,9 +1,29 @@
 var express = require('express');
 var router = express.Router();
 
+var rpi1 = false;
+var rpi2 = false;
+var title = 'Aquaberry control center';
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Aquaberry control center' });
+router.put('/rpi:id/:state', function(req, res, next) {
+	if (req.params.id == 1) {
+		rpi1 = req.params.state == 'on';
+		title = 'Aquaberry control center: STATUS OK'
+	} else if (req.params.id == 2) {
+		rpi2 = req.params.state == 'on';
+		title = 'Aquaberry control center: STATUS OK'
+	}
+
+	if (!rpi1 & !rpi2)
+		title = 'Aquaberry control center: FAILURE!'
+
+	res.send({ rpi1: rpi1, rpi2: rpi2 });
+});
+
+/* GET home page. */
+router.get('/status', function(req, res, next) {
+  res.render('index', { title: title, rpi1: rpi1, rpi2: rpi2 });
 });
 
 module.exports = router;
